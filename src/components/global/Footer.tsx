e5543f5f5dc9e0de45cf509d26ce1673fe5e6ed8
prateek-10 <prateeksibu333@gmail.com>
2024-08-03 02:44:17 +0530

    addition of initial navbar and footer files

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="border-t border-zinc-800  mt-10 lg:min-h-10 min-h-10 relative">
+      <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 pt-4 pb-8 md:py-8">
+        <div className="flex flex-row items-center gap-x-2 font-incognito">
+          <h3 className="font-inter">Built with ❤️ using</h3>
+          <a
+            href="https://nextjs.org"
+            rel="Next.js"
+            target="_blank"
+            className="flex items-center gap-x-2 text-white  hover:underline"
+          >
+            <Image src="/nextjs.svg" width={20} height={20} alt="nextjs logo" />
+            Next.js
+          </a>
+        </div>
+
+        <div className="flex flex-col lg:items-end items-center lg:text-start text-center">
+          <small className="text-zinc-400 font-incognito">
+            Copyright &copy;{" "}
+            <Link className="underline" href="">
+              Prateek kumar Nayak
+            </Link>{" "}
+            {new Date().getFullYear()} | All rights Reserved
+          </small>
+        </div>
+      </div>
+    </footer>
+  );
+}
