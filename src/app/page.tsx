f15dbddfc98afe5da7e77a79c7066f538f3e7ca1
prateek-10 <prateeksibu333@gmail.com>
2024-08-04 03:06:48 +0530

    home page section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
 import Image from "next/image";
+import MyPicture from "../../public/ProfilePic.jpeg";
+import { ImageWobble } from "../components/animations/imagewobble";
+import { Slide } from "../components/animations/slide";
+import { WobbleCard } from "../components/animations/wobblecard";
+import { BiLinkExternal } from "react-icons/bi";
+import Experience from "../pages/experience";
+import Education from "../pages/education";
+import Skills from "../pages/skills";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div id="home" className="container mx-auto">
+      <div className="relative lg:max-w-7xl mx-auto max-w-3xl md:px-16 px-6">
+        <section className="relative grid lg:grid-cols-custom grid-cols-1 gap-x-6 items-start justify-items-center">
+          <div className="order-1 md:gap-y-20 md:pt-0 pt-4 mt-14">
+            <Slide delay={0.14}>
+              <h1 className="font-incognito font-semibold tracking-tight sm:text-5xl text-2xl lg:leading-tight basis-1/2 mb-2">
+                Hey, I&apos;m Prateek kumar Nayak
+              </h1>
+              <h2 className="font-incognito font-semibold tracking-tight sm:text-4xl text-2xl lg:leading-tight basis-1/2 mb-4">
+                Full Stack Software Engineer
+              </h2>
+              <p className={"text-gray-200/80"}>
+                A dedicated full-stack engineer based in Bhubaneswar, Odisha,
+                specializing in building robust web applications with modern
+                technologies. I excel in designing and maintaining both frontend
+                and backend systems to deliver exceptional user experiences. My
+                expertise lies in crafting dynamic interfaces and efficient
+                server-side logic with optimized code. Utilizing the latest
+                tools and techniques, I create scalable, high-performance
+                solutions tailored to diverse business needs.
+              </p>
+              <br />
+              <br />
+              <WobbleCard
+                containerClassName="col-span-1 lg:col-span-2 h-full bg-cyan-800/90 min-h-fit lg:min-h-full"
+                className=""
+              >
+                <div className="w-full flex items-center justify-center ">
+                  If you ever see me out and about, feel free to stop by and say
+                  hello! Let&apos;s catch up over a coffee and dive into the
+                  latest trends in full-stack development or chat about the
+                  newest buzz in the tech world. 🚀
+                </div>
+              </WobbleCard>
+            </Slide>
+          </div>
+
+          <aside className="flex flex-col lg:justify-self-center justify-self-start gap-y-8 lg:order-1 order-none mb-12">
+            <Slide delay={0.1}>
+              <ImageWobble>
+                <Image
+                  className="rounded-2xl mb-4 object-cover max-h-auto min-h-96 bg-top"
+                  src={MyPicture}
+                  width={360}
+                  height={360}
+                  quality={100}
+                  alt="profile"
+                />
+              </ImageWobble>
+              <div className="flex flex-col text-center gap-y-4">
+                <div className="flex items-center gap-x-3">
+                  <a
+                    href=""
+                    rel="prateek"
+                    target="_blank"
+                    className="flex items-center justify-center text-center gap-x-2 basis-[90%] bg-gray-800/40 border border-transparent hover:border-zinc-400  rounded-md py-2 text-lg font-incognito font-semibold"
+                  >
+                    View Resume <BiLinkExternal className="text-base " />
+                  </a>
+                  <a
+                    href=""
+                    className="flex items-center justify-center text-center  hover:underline bg-gray-800/40  border border-transparent hover:border-zinc-400 rounded-md py-3 text-lg w-1/4"
+                    title="Download Resume"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="red"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </Slide>
+          </aside>
+        </section>
+        <section className="relative grid lg:grid-cols-1 grid-cols-1 mt-7 gap-x-6 items-start justify-items-start">
+          <Slide delay={0.14}>
+            <h1
+              id="experience"
+              className="font-incognito font-semibold tracking-tight sm:text-3xl text-xl lg:leading-tight basis-1/2 mb-4 mt-12"
+            >
+              Experience
+            </h1>
+            <div className="px-4">
+              <Experience />
+            </div>
+          </Slide>
+
+          <Slide delay={0.18}>
+            <h1
+              id="education"
+              className="font-incognito font-semibold tracking-tight sm:text-3xl text-xl lg:leading-tight basis-1/2 mb-4 mt-12"
+            >
+              Education
+            </h1>
+            <div className="px-4">
+              <Education />
+            </div>
+          </Slide>
+
+          <Slide delay={0.18}>
+            <h1
+              id="skills"
+              className="font-incognito font-semibold tracking-tight sm:text-3xl text-xl lg:leading-tight basis-1/2 mb-4 mt-12"
+            >
+              Technical Skills
+            </h1>
+            <div className="px-4">
+              <Skills />
+            </div>
+          </Slide>
+        </section>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
