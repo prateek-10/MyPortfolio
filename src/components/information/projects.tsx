78c2605899a3c8150f4827944a31ff7cade20579
prateek-10 <prateeksibu333@gmail.com>
2024-08-06 01:58:52 +0530

    adding project section

---
+++ src/components/information/projects.tsx
@@ -0,0 +1,18 @@
+export type ProjectProps = {
+  name: string;
+  logo: string;
+  tagline: string;
+  slug: string;
+  coverImage: string;
+};
+
+export const PROJECT: ProjectProps[] = [
+  {
+    name: "College Mart",
+    logo: "/icons/cm.png",
+    tagline:
+      "College Mart: Affordable Deals, Easy Transactions, Campus Connections!",
+    slug: "venmo-app",
+    coverImage: "/project-cover/cm-cover.png",
+  },
+];
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import PageHeading from "../components/shared/pageheading";
+import { Slide } from "../components/animations/slide";
+import EmptyState from "../components/animations/emptystate";
+import { ProjectWobble } from "../components/animations/projectwobble";
+import { PROJECT } from "../components/information/projects";
 
-const projects = () => {
-  return <div>projects</div>;
-};
+export default function Project() {
+  return (
+    <main className="max-w-7xl mx-auto md:px-16 px-6">
+      <PageHeading
+        title="Projects"
+        description="I've worked on these few projects over the years. All of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas on how it can be improved."
+        image="/project.gif"
+      />
 
-export default projects;
+      <Slide delay={0.1}>
+        {PROJECT.length > 0 ? (
+          <section className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mb-12">
+            {PROJECT.map((project) => (
+              <ProjectWobble key={project.slug}>
+                <Link
+                  href={`/projects/${project.slug}`}
+                  key={project.slug}
+                  className="flex items-center gap-x-4 bg-primary-bg border border-transparent p-4"
+                >
+                  <Image
+                    src={project.logo}
+                    width={50}
+                    height={50}
+                    alt={project.name}
+                    className="bg-zinc-800 rounded-md p-2"
+                  />
+                  <div>
+                    <h2 className="text-lg tracking-wide mb-1">
+                      {project.name}
+                    </h2>
+                    <div className="text-sm text-zinc-400">
+                      {project.tagline}
+                    </div>
+                  </div>
+                </Link>
+              </ProjectWobble>
+            ))}
+          </section>
+        ) : (
+          <EmptyState value="Projects" />
+        )}
+      </Slide>
+    </main>
+  );
+}
